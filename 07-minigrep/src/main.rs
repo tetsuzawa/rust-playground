8db493f77afbc9e780ee225ed9ae85bc8d5f7610
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-01-05 21:27:07 +0900

    Change: use struct for config

--- 07-minigrep/src/main.rs
+++ 07-minigrep/src/main.rs
@@ -4,25 +4,27 @@ use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    // println!("{:?}", args);
-    // println!("{:?}", type_of(env::args()));
-    // {}を探しています
-    // println!("Searching for {}", query);
-    // {}というファイルの中
-
-    // println!("In file {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+
+struct Config {
+    query: String,
+    filename: String,
 }
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
+}
+
+
