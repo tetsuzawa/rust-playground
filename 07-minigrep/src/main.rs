590ca8f56bc8ec2c3d348a992dcc0b282f5f4ce5
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-01-05 20:21:08 +0900

    Change to read query and filename

--- 07-minigrep/src/main.rs
+++ 07-minigrep/src/main.rs
@@ -2,8 +2,15 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
     println!("{:?}", args);
     println!("{:?}", type_of(env::args()));
+    // {}を探しています
+    println!("Searching for {}", query);
+    // {}というファイルの中
+    println!("In file {}", filename);
 }
 
 fn type_of<T>(_: T) -> String {
