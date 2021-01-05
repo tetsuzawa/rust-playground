9e9b5b28821e98306444bc54655525ea349410f6
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-01-05 21:56:25 +0900

    Add: error handling

--- 07-minigrep/src/main.rs
+++ 07-minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
-use std::fs::File;
+use std::process;
+use std::fs::{File, read_to_string};
 use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     let mut f = File::open(config.filename).expect("file not found");
 
@@ -20,10 +24,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
