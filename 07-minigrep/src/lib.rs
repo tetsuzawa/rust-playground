80fdcc52ce90944d81de6d27f61ce6e080d2edd1
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-01-07 09:21:02 +0900

    Add search function

--- 07-minigrep/src/lib.rs
+++ 07-minigrep/src/lib.rs
@@ -1,17 +1,6 @@
-use std::fs::{File, read_to_string};
+use std::fs::File;
 use std::io::Read;
 use std::error::Error;
-
-pub fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
 
 pub struct Config {
     pub query: String,
@@ -29,4 +18,47 @@ impl Config {
     }
 }
 
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line)
+        }
+    }
+    return result;
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
 
