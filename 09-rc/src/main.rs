3728bd9f45de560a9511fd2b67d303211f8aa8b9
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2022-02-17 22:56:03 +0900

    Add rc

---
+++ 09-rc/src/main.rs
@@ -0,0 +1,19 @@
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(3, Rc::clone(&a));
+
+    dbg!(a);
+    dbg!(b);
+    dbg!(c);
+}
